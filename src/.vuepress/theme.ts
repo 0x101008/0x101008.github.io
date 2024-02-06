ccef0c765383a2c70aa3ffe6923752c0c2f16ba3
0x100815 <shtnb@outlook.com>
2024-02-06 10:24:49 +0800

    plq

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -60,14 +60,17 @@ export default hopeTheme({
   // 在这里配置主题提供的插件
   plugins: {
     blog: true,
-
+      
     // 在启用之前需要安装 @waline/client
     // 警告: 这是一个仅供演示的测试服务器，在生产环境中请自行部署并使用自己的服务器！
     // comment: {
     //   provider: "Waline",
     //   serverURL: "https://waline-comment.vuejs.press",
     // },
-
+    comment: {
+         provider: "Waline",
+         serverURL: "https://plq-beta.vercel.app/",
+       },
     components: {
       components: ["Badge", "VPCard"],
     },
