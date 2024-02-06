c7921d7ba06546b34f8507805a290db925ff24d8
0x100815 <shtnb@outlook.com>
2024-02-06 09:08:35 +0800

    title

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,10 +3,10 @@ import theme from "./theme.js";
 
 export default defineUserConfig({
   base: "/",
-  
+
   lang: "zh-CN",
-  title: "博客演示",
-  description: "vuepress-theme-hope 的博客演示",
+  title: "shtの小窝",
+  description: "",
 
   theme,
 
