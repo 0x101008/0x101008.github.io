ed44f7a61d6f038b7b5153a013ab397a7c33b742
0x100815 <shtnb@outlook.com>
2024-02-06 10:46:27 +0800

    bigg

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -44,9 +44,9 @@ export default navbar([
       "strawberry",
     ],
   },
-  {
-    text: "V2 文档",
-    icon: "book",
-    link: "https://theme-hope.vuejs.press/zh/",
-  },
+  // {
+  //   text: "V2 文档",
+  //   icon: "book",
+  //   link: "https://theme-hope.vuejs.press/zh/",
+  // },
 ]);
