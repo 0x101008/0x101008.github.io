8c1265707d614cdbf38417e68f8c0477a6b27b4d
0x101008 <132063283+0x101008@users.noreply.github.com>
2024-03-03 02:27:36 +0000

    Deploying to gh-pages from @ 0x101008/0x101008.github.io@8795729dc2dc5622148804d37bde3a0c78b07cbd 🚀

--- assets/pageview-Brfc851Z.js
+++
@@ -1,7 +0,0 @@
-import{_ as t}from"./app-lUjHLzf3.js";const o=async()=>{try{const{pageviewCount:e}=await t(()=>import("./pageview_vuepress-plugin-comment2-BIHI7g3E.js"),__vite__mapDeps([]));return e({serverURL:COMMENT_OPTIONS.serverURL})}catch{console.error("@waline/client is not installed!")}};export{o as updatePageview};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = []
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
