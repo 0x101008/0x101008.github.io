126febe6cb773bd12c42b6fa1645b497b2157053
0x101008 <132063283+0x101008@users.noreply.github.com>
2024-03-03 03:25:02 +0000

    Deploying to gh-pages from @ 0x101008/0x101008.github.io@19749c1f0922b71070a3b3b6e3765a999e0342a0 🚀

--- assets/pageview-DEkuRYXH.js
+++
@@ -1,7 +0,0 @@
-import{_ as r}from"./app-D5ZtD7V0.js";var o={provider:"Waline",dark:"auto",serverURL:"https://comment.bloggersht.com.cn",comment:!0,pageview:!0,copyright:!0};const i=async()=>{try{const{pageviewCount:e}=await r(()=>import("./app-D5ZtD7V0.js").then(t=>t.O),__vite__mapDeps([]));return e({serverURL:o.serverURL})}catch{console.error("@waline/client is not installed!")}};export{i as updatePageview};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = []
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
