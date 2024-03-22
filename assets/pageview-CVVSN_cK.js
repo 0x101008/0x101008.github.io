c2981983f82b40fe8d14d735934ac39c9a3b60a7
0x101008 <132063283+0x101008@users.noreply.github.com>
2024-03-22 11:39:05 +0000

    Deploying to gh-pages from @ 0x101008/0x101008.github.io@228a637adde60f12400f58ee13ba6ef2c4de6681 🚀

--- assets/pageview-CVVSN_cK.js
+++
@@ -1,7 +0,0 @@
-import{_ as i}from"./app-CYHcmHlj.js";var o={provider:"Waline",dark:"auto",serverURL:"https://comment.bloggersht.com.cn",reaction:["/emoji/img_love.png","/emoji/gif_hutao_bite.gif","/emoji/gif_thump.gif","/emoji/gif_tui.gif","/emoji/img_good.png"],comment:!0,pageview:!0,copyright:!0};const a=async()=>{try{const{pageviewCount:e}=await i(()=>import("./app-CYHcmHlj.js").then(t=>t.K),__vite__mapDeps([]));return e({serverURL:o.serverURL})}catch{console.error("@waline/client is not installed!")}};export{a as updatePageview};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = []
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
