f6bc594201ff5a58e20dcae9e2f101b9b489346a
0x100815 <shtnb@outlook.com>
2024-03-22 20:14:09 +0800

    side

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -2,5 +2,12 @@ import { navbar } from "vuepress-theme-hope";
 
 export default navbar([
   "/",
-  { text: '友情链接', icon: '', link: '#'}
+  { text: '友情链接', icon: '', link: '#'},
+  {
+    text:'游戏',
+    icon:'#',
+    children:[
+      'game.html'
+    ]
+  }
 ]);
